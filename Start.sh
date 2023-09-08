a15987b913c46ad46d99f566c5ba1e78cac02c34
ItsRichardScull <totallynotrichardscull@gmail.com>
2023-09-08 15:41:15 +0300

    feat: Fixed Start.sh file

--- Start.sh
+++ Start.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if ! command -v node &> /dev/null; then
-  echo "Node.js is not installed. Installing Node.js..."
-  curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-  sudo apt-get install -y nodejs
-fi
-
 if ! command -v npm &> /dev/null; then
-  echo "npm is not installed. Installing npm..."
-  sudo apt-get install -y npm
+  echo "nvm is not installed. Installing nvm...."
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+  source ~/.bashrc
+  nvm install --lts
+  nvm use --lts
 fi
 
-npm install --no-audit
+npm i --no-audit
 npm run buildAndStart
+
+
